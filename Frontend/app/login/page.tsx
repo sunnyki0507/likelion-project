0405927efb1667fb8db87908ccc5004901df9c28
J Yoon <junsang234@gmail.com>
2025-04-20 14:49:50 -0700

    basic setup

---
+++ Frontend/app/login/page.tsx
@@ -0,0 +1,20 @@
+
+
+export default function Login(){
+
+
+    return (
+        <>
+            {/* background */}
+            <div className="flex-1 flex items-center justify-center">
+
+                {/* loging box */}
+                <div className="rounded-3xl w-4xl h-[600px] bg-white">
+                    
+                </div>
+
+            </div>
+        </>
+    )
+
+}
---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    theme: {
+      extend: {
+        // ...
+      },
+      colors:{
+        
+      }
+    },
+    plugins: [],
+  }
+  
