76bb2d1e1f3f94e75b6a2dce1f500773eeaa5ac1
J Yoon <junsang234@gmail.com>
2025-05-08 15:08:01 -0700

    .

--- Frontend/app/favorites/page.tsx
+++ Frontend/app/favorites/page.tsx
@@ -1,21 +1,9 @@
-import { getRestaurants } from "../(api)/getRestaurants";
-import ViewSelector from "../(components)/ViewSelector";
 
-const sampleTag: Tags = {
-    location: "irvine",
-    category: "Thai",
-    distance: 5,
-    ratings: 4,
-    delivery: true,
-    vegan: false,
-}
 
-export default async function HomePage() {
-    const restaurants = await getRestaurants({ tags: sampleTag, size: 5 });
+export default async function Favorites() {
 
     return (
         <>
-            <ViewSelector initRestaurants={restaurants}/>
         </>
     );
 }
